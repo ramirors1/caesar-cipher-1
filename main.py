99902fb1fad711801756b7d8ad6e3f5b51ca406f
Ramiro Ramirez-Sanchez <45070898+ramirors1@users.noreply.github.com>
2022-10-03 14:08:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+# encrypt function that takes the 'text' and 'shift' as inputs.
+def encrypt(input_text, shift_amount):
+    cipher_text = ""
+    for letter in input_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
+    #shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    
+    #Call the encrypt function and pass in the user inputs. 
+encrypt(input_text=text, shift_amount=shift)
